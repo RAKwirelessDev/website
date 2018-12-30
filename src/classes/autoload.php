62c7d81eee4cfaee028734022d1ff3615263de5c
Phoenix Eve Aspacio <aspaciop@gmail.com>
2018-12-31 00:34:57 +0800

    autload consistency

--- src/classes/autoload.php
+++ src/classes/autoload.php
@@ -5,7 +5,7 @@ spl_autoload_register(
         $class_name = str_replace('\\', DIRECTORY_SEPARATOR, $class_name);
         $file = __DIR__ . DIRECTORY_SEPARATOR . $class_name.".php";
         if (is_readable($file)) {
-            require_once $file;
+            require_once($file);
         } else {
             throw new \Exception("Undefined Class: \"$class_name\".");
         }
