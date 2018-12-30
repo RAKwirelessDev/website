9bdfc4a09eb016a8076d421551886ae987c1c474
Phoenix Eve Aspacio <aspaciop@gmail.com>
2018-12-31 01:05:21 +0800

    about fix

---
+++ src/views/segments/about/company-gallery.php
@@ -0,0 +1,71 @@
+<div class="col-lg-5 mb-3 mb-lg-0">
+
+    <!-- Image Gallery -->
+    <div class="row mx-gutters-2">
+        <div class="col-5 align-self-end px-2 mb-3">
+            <!-- Fancybox -->
+            <a class="mw-mc js-fancybox u-media-viewer" href="javascript:;" data-src="/assets/img/rakwireless/company1.jpg" data-fancybox="lightbox-gallery-hidden" data-caption="Front in frames - image #01" data-speed="700">
+                <img class="img-fluid rounded" src="/assets/img/rakwireless/company1.jpg" alt="Image Description">
+
+                <span class="u-media-viewer__container">
+                    <span class="u-media-viewer__icon">
+                        <span class="fas fa-plus u-media-viewer__icon-inner"></span>
+                    </span>
+                </span>
+            </a>
+            <!-- End Fancybox -->
+        </div>
+
+        <div class="col-7 px-2 mb-3">
+            <!-- Fancybox -->
+            <a class="mw-mc js-fancybox u-media-viewer" href="javascript:;" data-src="/assets/img/rakwireless/company2.jpg" data-fancybox="lightbox-gallery-hidden" data-caption="Front in frames - image #02" data-speed="700">
+                <img class="img-fluid rounded" src="/assets/img/rakwireless/company2.jpg" alt="Image Description">
+
+                <span class="u-media-viewer__container">
+                    <span class="u-media-viewer__icon">
+                        <span class="fas fa-plus u-media-viewer__icon-inner"></span>
+                    </span>
+                </span>
+            </a>
+            <!-- End Fancybox -->
+        </div>
+
+        <div class="col-5 offset-1 px-2 mb-3">
+            <!-- Fancybox -->
+            <a class="mw-mc js-fancybox u-media-viewer" href="javascript:;" data-src="/assets/img/rakwireless/company3.jpg" data-fancybox="lightbox-gallery-hidden" data-caption="Front in frames - image #03" data-speed="700">
+                <img class="img-fluid rounded" src="/assets/img/rakwireless/company3.jpg" alt="Image Description">
+
+                <span class="u-media-viewer__container">
+                    <span class="u-media-viewer__icon">
+                        <span class="fas fa-plus u-media-viewer__icon-inner"></span>
+                    </span>
+                </span>
+            </a>
+            <!-- End Fancybox -->
+        </div>
+
+        <div class="col-5 px-2 mb-3">
+            <!-- Fancybox -->
+            <a class="mw-mc js-fancybox u-media-viewer" href="javascript:;" data-src="/assets/img/rakwireless/company4.jpg" data-fancybox="lightbox-gallery-hidden" data-caption="Front in frames - image #04" data-speed="700">
+                <img class="img-fluid rounded" src="/assets/img/rakwireless/company4.jpg" alt="Image Description">
+
+                <span class="u-media-viewer__container">
+                    <span class="u-media-viewer__icon">
+                        <span class="fas fa-plus u-media-viewer__icon-inner"></span>
+                    </span>
+                </span>
+            </a>
+            <!-- End Fancybox -->
+        </div>
+    </div>
+    <!-- End Image Gallery -->
+
+    <!-- SVG Background Shape -->
+    <div id="SVGbgShapeID1" class="svg-preloader w-100 content-centered-y z-index-n1">
+        <figure class="ie-soft-triangle-shape">
+            <img class="js-svg-injector" src="../../assets/svg/components/soft-triangle-shape.svg" alt="Image Description" data-parent="#SVGbgShapeID1">
+        </figure>
+    </div>
+    <!-- End SVG Background Shape -->
+
+</div>
