cc4a62382208974c0971c249abac7bb8811d4927
Phoenix Eve Aspacio <aspaciop@gmail.com>
2018-12-30 22:59:33 +0800

    front to rak

--- src/views/snippets/segments/header/navigation-logo.php
+++ src/views/snippets/segments/header/navigation-logo.php
@@ -1,5 +1,5 @@
 <!-- Logo -->
-<a class="navbar-brand u-header__navbar-brand u-header__navbar-brand-center" href="/" aria-label="Front">
+<a class="navbar-brand u-header__navbar-brand u-header__navbar-brand-center" href="/" aria-label="RAK">
     <img style="height: 40px; width: auto;" src="/assets/img/rakwireless/logo.png" />
     <span style="color: #0049a0;" class="u-header__navbar-brand-text">RAK</span>
 </a>
