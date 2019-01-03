ea8633777914656a0bb3e8ac487a3aad3fa87e08
Phoenix Eve Aspacio <aspaciop@gmail.com>
2019-01-03 13:01:29 +0800

    more fixes

--- assets/js/theme-custom.js
+++ assets/js/theme-custom.js
@@ -1,1 +1,33 @@
-// File for your custom JavaScript
+(function($){
+  $.fn.fullHeight = function(){
+    var self = this;
+    var fullHeightReset = function(){
+		var elements = self.children();
+		for (var i = 0; i < elements.length; i++) {
+			$(elements[i]).css({
+				'height': 'auto'
+			});
+		}
+		setTimeout(function(){
+			for (var i = 0; i < elements.length; i++) {
+				$(elements[i]).css({
+					'height': self.height()
+				});
+			}
+		}, 500);
+    }
+	fullHeightReset();
+  }
+})(jQuery);
+$(document).ready(function() {
+	var windowWidth = $(window).width();
+	var windowHeight = $(window).height();
+    $('#carousel-fullheight > .slick-list.draggable > .slick-track').fullHeight();
+	$(window).resize(function() {
+		if ($(window).width() != windowWidth || $(window).height() != windowHeight) {
+			$('#carousel-fullheight > .slick-list.draggable > .slick-track').fullHeight();
+			windowWidth = $(window).width();
+			windowHeight = $(window).height();
+		}
+	});
+});
