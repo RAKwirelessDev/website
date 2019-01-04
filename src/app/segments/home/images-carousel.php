445968f4074949bb3705bb475c4050e48823cdaa
Phoenix Eve Aspacio <aspaciop@gmail.com>
2019-01-04 10:08:19 +0800

    minor changes

--- src/app/segments/home/images-carousel.php
+++ src/app/segments/home/images-carousel.php
@@ -1,14 +1,22 @@
 <?php
 
 $data->carousel = [
-    'product' => [
-        'name' => 'LoRa/LoRaWAN Gateway',
+    'LoRa/LoRaWAN Gateway' => [
         'tag_line' => 'Revolutionary IoT Access Technology - LoRa Gateway Module',
-        'description' => '
-        - Based on Semtech\'s SX1301<br>
-        - Frequency: 443MHz/868MHz/915MHz<br>
-        - open 8 channels uplink and 1 channel downlink for Makers
-        ',
+        'description' => '- Based on Semtech\'s SX1301<br>- Frequency: 443MHz/868MHz/915MHz<br>- open 8 channels uplink and 1 channel downlink for Makers',
+        'images' => [
+            '/assets/img/rakwireless/01_small.png',
+            '/assets/img/rakwireless/01_small.png'
+        ],
+        'store_link' => 'https://www.aliexpress.com/store/product/Pilot-Gate-way-connect-to-the-TTN-Network-Server-and-IoT-in-a-Box-application-cloud/2805180_32951841630.html?spm=a2g1y.12024536.productList_11474674.pic_5',
+
+        'background-style' => [
+            'image' => '/assets/img/rakwireless/01_small.png'
+        ]
+    ],
+    'LoRa/LoRaWAN Gateway Entry 2' => [
+        'tag_line' => 'Revolutionary IoT Access Technology - LoRa Gateway Module',
+        'description' => '- Based on Semtech\'s SX1301<br>- Frequency: 443MHz/868MHz/915MHz<br>- open 8 channels uplink and 1 channel downlink for Makers<br>- Cool product!!!',
         'images' => [
             '/assets/img/rakwireless/01_small.png',
             '/assets/img/rakwireless/01_small.png'
--- src/views/segments/home/images-carousel.php
+++ src/views/segments/home/images-carousel.php
@@ -5,37 +5,34 @@
 
 <?php
 
-foreach ($data->carousel as $slide_type => $slide_data) {
-    if ($slide_type === 'product') {
-        echo '<div class="min-height-100vh js-slide bg-img-hero-center gradient-half-warning-v3">';
-        echo '<div class="container space-top-md-3 space-2 mt-5">';
-        echo '<div class="row justify-content-md-between">';
-        echo '<div class="col-md-6 mb-7 mb-md-0">';
-        echo '<div class="js-slick-carousel u-slick" data-infinite="true" data-arrows-classes="u-slick__arrow-classic u-slick__arrow-centered--y rounded-circle" data-arrow-left-classes="fas fa-arrow-left u-slick__arrow-classic-inner u-slick__arrow-classic-inner--left" data-arrow-right-classes="fas fa-arrow-right u-slick__arrow-classic-inner u-slick__arrow-classic-inner--right" data-pagi-classes="text-center u-slick__pagination u-slick__pagination--white mt-3 mb-0">';
-        foreach ($slide_data['images'] as $image_path) {
-            //$image_path = Core\Commmons::slugify($image_path);
-            echo '<div class="js-slide px-7 my-4">';
-            echo '<img class="img-fluid" src="'.$image_path.'" alt="'.$slide_data['name'].'">';
-            echo '</div>';  
-        }  
-        echo '</div>';
-        echo '</div>';
-        echo '<div class="col-md-6">';
-        echo '<div class="mb-5">';
-        echo '<h1 class="text-white font-weight-normal">'.$slide_data['name'].'</h1>';
-        if (!empty($slide_data['tag_line'])) {
-            echo '<span class="d-block h5 text-white mb-4">'.$slide_data['tag_line'].'</span>';
-        }
-        echo '<p class="text-white">'.$slide_data['description'].'</p>';
-        echo '</div>';
-        echo '<a class="btn btn-soft-white transition-3d-hover" href="'.$slide_data['store_link'].'">';
-        echo '<span class="fas fa-cart-arrow-down mr-2"></span> Buy Now';
-        echo '</a>';
-        echo '</div>';
-        echo '</div>';
-        echo '</div>';
-        echo '</div>';
+foreach ($data->carousel as $product_name => $product_info) {
+    echo '<div class="min-height-100vh js-slide bg-img-hero-center gradient-half-warning-v3">';
+    echo '<div class="container space-top-md-3 space-2 mt-5">';
+    echo '<div class="row justify-content-md-between">';
+    echo '<div class="col-md-6 mb-7 mb-md-0">';
+    echo '<div class="js-slick-carousel u-slick" data-infinite="true" data-arrows-classes="u-slick__arrow-classic u-slick__arrow-centered--y rounded-circle" data-arrow-left-classes="fas fa-arrow-left u-slick__arrow-classic-inner u-slick__arrow-classic-inner--left" data-arrow-right-classes="fas fa-arrow-right u-slick__arrow-classic-inner u-slick__arrow-classic-inner--right" data-pagi-classes="text-center u-slick__pagination u-slick__pagination--white mt-3 mb-0">';
+    foreach ($product_info['images'] as $image_path) {
+        echo '<div class="js-slide px-7 my-4">';
+        echo '<img class="img-fluid" src="'.$image_path.'" alt="'.$product_name.'">';
+        echo '</div>';  
+    }  
+    echo '</div>';
+    echo '</div>';
+    echo '<div class="col-md-6">';
+    echo '<div class="mb-5">';
+    echo '<h1 class="text-white font-weight-normal">'.$product_name.'</h1>';
+    if (!empty($product_info['tag_line'])) {
+        echo '<span class="d-block h5 text-white mb-4">'.$product_info['tag_line'].'</span>';
     }
+    echo '<p class="text-white">'.$product_info['description'].'</p>';
+    echo '</div>';
+    echo '<a class="btn btn-soft-white transition-3d-hover" href="'.$product_info['store_link'].'">';
+    echo '<span class="fas fa-cart-arrow-down mr-2"></span> Buy Now';
+    echo '</a>';
+    echo '</div>';
+    echo '</div>';
+    echo '</div>';
+    echo '</div>';
 }
 
 ?>
