3995e21311ec1aba53428349c1e2f1c136052ef6
Phoenix Eve Aspacio <aspaciop@gmail.com>
2019-01-04 09:56:29 +0800

    feature: carousel automate

--- src/views/segments/home/images-carousel.php
+++ src/views/segments/home/images-carousel.php
@@ -2,73 +2,44 @@
 <div class="container-fluid">
     <div id="carousel-fullheight" class="js-slick-carousel u-slick gradient-half-warning-v3" data-autoplay="true" data-speed="5000" data-pagi-classes="text-center u-slick__pagination u-slick__pagination--white position-absolute right-0 bottom-0 left-0">
 
-        <div class="min-height-100vh js-slide bg-img-hero-center gradient-half-warning-v3">
-            <div class="container space-4">
-                <div class="w-lg-30">
-                    <h4 class="display-4 text-white font-weight-semi-bold mb-3" data-scs-animation-in="fadeInUp">High<br>quality</h4>
-                    <p class="lead text-white" data-scs-animation-in="fadeInUp">We aim high at being focused on hight quality materials.</p>
-                </div>
-            </div>
-        </div>
 
-        <div class="min-height-100vh js-slide bg-img-hero-center gradient-half-warning-v3">
-            <div class="container space-top-md-3 space-2">
+<?php
 
-                <div class="row justify-content-md-between">
-                    <!-- Item Image -->
-                    <div class="col-md-6 mb-7 mb-md-0">
-                        <div class="js-slick-carousel u-slick" data-infinite="true" data-arrows-classes="u-slick__arrow-classic u-slick__arrow-centered--y rounded-circle" data-arrow-left-classes="fas fa-arrow-left u-slick__arrow-classic-inner u-slick__arrow-classic-inner--left" data-arrow-right-classes="fas fa-arrow-right u-slick__arrow-classic-inner u-slick__arrow-classic-inner--right" data-pagi-classes="text-center u-slick__pagination u-slick__pagination--white mt-7 mb-0">
-                            <div class="js-slide px-7 my-4">
-                                <img class="img-fluid" src="/assets/img/rakwireless/01.png" alt="Image Description">
-                            </div>
-                            <div class="js-slide px-7 my-4">
-                                <img class="img-fluid" src="/assets/img/rakwireless/03.png" alt="Image Description">
-                            </div>
-                            <div class="js-slide px-7 my-4">
-                                <img class="img-fluid" src="../../assets/img/mockups/img1.png" alt="Image Description">
-                            </div>
-                        </div>
-                    </div>
-                    <!-- End Item Image -->
+foreach ($data->carousel as $slide_type => $slide_data) {
+    if ($slide_type === 'product') {
+        echo '<div class="min-height-100vh js-slide bg-img-hero-center gradient-half-warning-v3">';
+        echo '<div class="container space-top-md-3 space-2 mt-5">';
+        echo '<div class="row justify-content-md-between">';
+        echo '<div class="col-md-6 mb-7 mb-md-0">';
+        echo '<div class="js-slick-carousel u-slick" data-infinite="true" data-arrows-classes="u-slick__arrow-classic u-slick__arrow-centered--y rounded-circle" data-arrow-left-classes="fas fa-arrow-left u-slick__arrow-classic-inner u-slick__arrow-classic-inner--left" data-arrow-right-classes="fas fa-arrow-right u-slick__arrow-classic-inner u-slick__arrow-classic-inner--right" data-pagi-classes="text-center u-slick__pagination u-slick__pagination--white mt-3 mb-0">';
+        foreach ($slide_data['images'] as $image_path) {
+            //$image_path = Core\Commmons::slugify($image_path);
+            echo '<div class="js-slide px-7 my-4">';
+            echo '<img class="img-fluid" src="'.$image_path.'" alt="'.$slide_data['name'].'">';
+            echo '</div>';  
+        }  
+        echo '</div>';
+        echo '</div>';
+        echo '<div class="col-md-6">';
+        echo '<div class="mb-5">';
+        echo '<h1 class="text-white font-weight-normal">'.$slide_data['name'].'</h1>';
+        if (!empty($slide_data['tag_line'])) {
+            echo '<span class="d-block h5 text-white mb-4">'.$slide_data['tag_line'].'</span>';
+        }
+        echo '<p class="text-white">'.$slide_data['description'].'</p>';
+        echo '</div>';
+        echo '<a class="btn btn-soft-white transition-3d-hover" href="'.$slide_data['store_link'].'">';
+        echo '<span class="fas fa-cart-arrow-down mr-2"></span> Buy Now';
+        echo '</a>';
+        echo '</div>';
+        echo '</div>';
+        echo '</div>';
+        echo '</div>';
+    }
+}
 
-                    <!-- Content -->
-                    <div class="col-md-5">
-                        <div class="mb-5">
-                            <h1 class="text-white font-weight-normal">Pilot Gateway</h1>
-                            <span class="d-block h3 text-white mb-3">$25</span>
-                            <p class="text-white">Our Front Design Makeup collection – to create your coolest look, then switch it up again tomorrow. Make every day, night and everything in-between as extraordinary as you are.</p>
-                        </div>
+?>
 
-                        <!-- Size Custom Select -->
-                        <div class="w-lg-75 mb-5">
-                            <div class="d-flex justify-content-between">
-                                <label class="h6 small d-block text-uppercase text-white">
-                                    Size
-                                </label>
-                                <a class="h6 small d-block text-uppercase text-light" href="#shoppingCartModal" role="button" data-modal-target="#shoppingCartModal" data-overlay-color="#111722">
-                  Size guide
-                </a>
-                            </div>
-
-                            <select class="custom-select">
-                                <option selected>Please select</option>
-                                <option value="sizeSelect1">Small</option>
-                                <option value="sizeSelect2">Medium</option>
-                                <option value="sizeSelect3">Large</option>
-                                <option value="sizeSelect4">Extra large</option>
-                            </select>
-                        </div>
-                        <!-- End Size Custom Select -->
-
-                        <a class="btn btn-soft-white transition-3d-hover" href="cart.html">
-                            <span class="fas fa-cart-arrow-down mr-2"></span> Add to Cart
-                        </a>
-                    </div>
-                    <!-- End Content -->
-                </div>
-
-            </div>
-        </div>
 
     </div>
 </div>
