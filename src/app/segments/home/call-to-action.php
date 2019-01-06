54c4aa4ba08916cef2dcc2210d300c2aee8e9c51
Phoenix Eve Aspacio <aspaciop@gmail.com>
2019-01-06 21:57:08 +0800

    feature: home cta automate

---
+++ src/app/segments/home/call-to-action.php
@@ -0,0 +1,60 @@
+<?php
+
+$data->call_to_action = [
+
+    [
+        'title' => 'Why choose RAK?',
+        'tagline' => '<span class="font-weight-semi-bold">Customization</span> – our expertise meet your scenario Requirement',
+        'item_set' => [
+            [
+                'image' => '/assets/img/rakwireless/why_choose_rak_1.png',
+                'title' => 'Enhanced Hardware',
+                'tagline' => 'Multiple dimension and size<br>Flexible Power Mode<br>Longer Range coverage'
+            ],
+            [
+                'image' => '/assets/img/rakwireless/why_choose_rak_2.png',
+                'title' => 'Complete Software Solution',
+                'tagline' => 'Embedded firmware<br>Network stack<br>Apps<br>Cloud'
+            ],
+            [
+                'image' => '/assets/img/rakwireless/why_choose_rak_3.png',
+                'title' => 'Universal Scenario',
+                'tagline' => 'Multiple interface<br>Multiple AT Commands<br>Multiple IoT Protocols'
+            ]
+        ]
+    ],
+
+    [
+        'tagline' => '<span class="font-weight-semi-bold">Smart++</span> – Seamless Integration to "Smart"',
+        'item_set' => [
+            [
+                'image' => '/assets/img/rakwireless/why_choose_rak_4.png',
+                'title' => 'System Software',
+                'tagline' => 'WIFi System Design<br>Network Stack<br>Embedded Hardware'
+            ],
+            [
+                'image' => '/assets/img/rakwireless/why_choose_rak_5.png',
+                'title' => 'Cloud Consultancy',
+                'tagline' => 'IoT Protocol<br>Build product to "work with"<br>Cloud Deployment'
+            ]
+        ]
+    ],
+
+    [
+        'tagline' => 'Qualified Service',
+        'item_set' => [
+            [
+                'image' => '/assets/img/rakwireless/why_choose_rak_6.png',
+                'title' => 'High Standard Manufacture'
+            ],
+            [
+                'image' => '/assets/img/rakwireless/why_choose_rak_7.png',
+                'title' => 'ERP Management'
+            ],
+            [
+                'image' => '/assets/img/rakwireless/why_choose_rak_8.png',
+                'title' => 'Quick Support'
+            ]
+        ]
+    ]
+];
--- src/views/segments/home/call-to-action.php
+++ src/views/segments/home/call-to-action.php
@@ -1,182 +1,53 @@
 <!-- CTA Section -->
 <div class="text-white position-relative gradient-half-primary-v1 text-center z-index-2 overflow-hidden">
     <div class="container space-1">
-        <h3 class="h2 text-info font-weight-semi-bold mb-3">Why choose RAK?</h3>
-        <p class="lead text-white mb-5">
-            <span class="font-weight-semi-bold">Customization</span> – our expertise meet your scenario Requirement
-        </p>
 
-        <!-- Process Section -->
-        <div class="row">
-            <div class="col-md-4 mb-7 mb-md-0">
-                <!-- Process -->
-                <div class="text-center">
-                    <div class="position-relative">
-                        <div class="u-indicator-dots min-height-155 mb-2">
+<?php
 
-                            <img src="/assets/img/rakwireless/why_choose_rak_1.png">
+foreach ($data->call_to_action as $segment) {
+    if (!empty($segment['title'])) {
+        echo '<h3 class="h2 text-info font-weight-semi-bold mb-3">'.$segment['title'].'</h3>';
+    }
+    if (!empty($segment['tagline'])) {
+        echo '<p class="lead text-white mb-5 mt-10">';
+        echo $segment['tagline'];
+        echo '</p>';
+    }
+    if (!empty($segment['item_set']) && is_array($segment['item_set'])) {
+        echo '<div class="row">';
+        $item_total = count($segment['item_set']);
+        $item_md = 12/($item_total);
+        $item_count = 0;
+        foreach ($segment['item_set'] as $item) {
+            $item_count += 1;
+            echo '<div class="col-md-'.$item_md.' mb-7 mb-md-0">';
+            echo '<div class="text-center">';
 
-                        </div>
-                    </div>
+            if (!empty($item['image'])) {
+                echo '<div class="position-relative">';
+                echo '<div class="'.($item_total > $item_count ? 'u-indicator-dots ' : '' ).'min-height-155 mb-2">';
+                echo '<img src="'.$item['image'].'">';
+                echo '</div>';
+                echo '</div>';
+            }
+            if (!empty($item['title'])) {
+                echo '<h3 class="h5">';
+                echo $item['title'];
+                echo '</h3>';
+            }
+            if (!empty($item['tagline'])) {
+                echo '<p class="text-white mb-md-0">';
+                echo $item['tagline'];
+                echo '</p>';
+            }
+            echo '</div>';
+            echo '</div>';
+        }
+        echo '</div>';
+    }
+}
 
-                    <h3 class="h5">Enhanced Hardware</h3>
-                    <p class="text-white mb-md-0">
-                        Multiple dimension and size<br>
-                        Flexible Power Mode<br>
-                        Longer Range coverage
-                    </p>
-                </div>
-                <!-- End Process -->
-            </div>
-
-            <div class="col-md-4 mb-7 mb-md-0">
-                <!-- Process -->
-                <div class="text-center">
-                    <div class="position-relative">
-                        <div class="u-indicator-dots min-height-155 mb-2">
-
-                            <img src="/assets/img/rakwireless/why_choose_rak_2.png">
-
-                        </div>
-                    </div>
-
-                    <h3 class="h5">Complete Software Solution</h3>
-                    <p class="text-white mb-md-0">
-                        Embedded firmware<br>
-                        Network stack<br>
-                        Apps<br>
-                        Cloud
-                    </p>
-                    <!-- End Process -->
-                </div>
-            </div>
-
-            <div class="col-md-4 mb-7 mb-md-0">
-                <!-- Process -->
-                <div class="text-center">
-                    <div class="min-height-155 mb-2">
-
-                        <img src="/assets/img/rakwireless/why_choose_rak_3.png">
-
-                    </div>
-
-                    <h3 class="h5">Universal Scenario</h3>
-                    <p class="text-white mb-md-0">
-                        Multiple interface<br>
-                        Multiple AT Commands<br>
-                        Multiple IoT Protocols
-                    </p>
-                    <!-- End Process -->
-                </div>
-            </div>
-        </div>
-        <!-- End Process Section -->
-
-        <p class="lead text-white mb-5 mt-10">
-            <span class="font-weight-semi-bold">Smart++</span> – Seamless Integration to "Smart"
-        </p>
-        <!-- Process Section -->
-        <div class="row">
-            <div class="col-md-6 mb-7 mb-md-0">
-                <!-- Process -->
-                <div class="text-center">
-                    <div class="position-relative">
-                        <div class="u-indicator-dots min-height-155 mb-2">
-
-                            <img src="/assets/img/rakwireless/why_choose_rak_4.png">
-
-                        </div>
-                    </div>
-
-                    <h3 class="h5">System Software</h3>
-                    <p class="text-white mb-md-0">
-                        WIFi System Design<br>
-                        Network Stack<br>
-                        Embedded Hardware
-                    </p>
-                </div>
-                <!-- End Process -->
-            </div>
-
-            <div class="col-md-6 mb-7 mb-md-0">
-                <!-- Process -->
-                <div class="text-center">
-                    <div class="position-relative">
-                        <div class="min-height-155 mb-2">
-
-                            <img src="/assets/img/rakwireless/why_choose_rak_5.png">
-
-                        </div>
-                    </div>
-
-                    <h3 class="h5">Cloud Consultancy</h3>
-                    <p class="text-white mb-md-0">
-                        IoT Protocol<br>
-                        Build product to "work with"<br>
-                        Cloud Deployment
-                    </p>
-                    <!-- End Process -->
-                </div>
-            </div>
-
-        </div>
-        <!-- End Process Section -->
-
-        <p class="lead text-white mb-5 mt-10">
-            <span class="font-weight-semi-bold">Qualified Service</span>
-        </p>
-        <!-- Process Section -->
-        <div class="row">
-            <div class="col-md-4 mb-7 mb-md-0">
-                <!-- Process -->
-                <div class="text-center">
-                    <div class="position-relative">
-                        <div class="u-indicator-dots min-height-155 mb-2">
-
-                            <img src="/assets/img/rakwireless/why_choose_rak_6.png">
-
-                        </div>
-                    </div>
-
-                    <h3 class="h5">High Standard Manufacture</h3>
-                    <!-- <p class="text-white mb-md-0"></p> -->
-                </div>
-                <!-- End Process -->
-            </div>
-
-            <div class="col-md-4 mb-7 mb-md-0">
-                <!-- Process -->
-                <div class="text-center">
-                    <div class="position-relative">
-                        <div class="u-indicator-dots min-height-155 mb-2">
-
-                            <img src="/assets/img/rakwireless/why_choose_rak_7.png">
-
-                        </div>
-                    </div>
-
-                    <h3 class="h5">ERP Management</h3>
-                    <!-- <p class="text-white mb-md-0"></p> -->
-                    <!-- End Process -->
-                </div>
-            </div>
-
-            <div class="col-md-4">
-                <!-- Process -->
-                <div class="text-center">
-                    <div class="min-height-155 mb-2">
-
-                        <img src="/assets/img/rakwireless/why_choose_rak_8.png">
-
-                    </div>
-
-                    <h3 class="h5">Quick Support</h3>
-                    <!-- <p class="text-white mb-md-0"></p> -->
-                    <!-- End Process -->
-                </div>
-            </div>
-        </div>
-        <!-- End Process Section -->
+?>
 
     </div>
 
