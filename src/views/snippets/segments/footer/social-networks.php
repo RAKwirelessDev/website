6f904764ed0f5b32e8daa3248f917558a652c08d
Phoenix Eve Aspacio <aspaciop@gmail.com>
2019-01-07 22:09:35 +0800

    social network links

--- src/views/snippets/segments/footer/social-networks.php
+++ src/views/snippets/segments/footer/social-networks.php
@@ -3,23 +3,27 @@
 <!-- Social Networks -->
 <ul class="list-inline mb-0">
     <li class="list-inline-item">
-        <a class="btn btn-sm btn-icon btn-soft-secondary" href="#">
+        <a class="btn btn-sm btn-icon btn-soft-secondary" href="https://www.facebook.com/RakwirelessIoT/">
             <span class="fab fa-facebook-f btn-icon__inner"></span>
         </a>
     </li>
     <li class="list-inline-item">
-        <a class="btn btn-sm btn-icon btn-soft-secondary" href="#">
-            <span class="fab fa-google btn-icon__inner"></span>
+        <a class="btn btn-sm btn-icon btn-soft-secondary" href="https://www.instagram.com/rak_wireless/">
+            <span class="fab fa-instagram btn-icon__inner"></span>
+        </a>
+    </li>    <li class="list-inline-item">
+        <a class="btn btn-sm btn-icon btn-soft-secondary" href="https://www.linkedin.com/company/rak-wireless">
+            <span class="fab fa-linkedin btn-icon__inner"></span>
         </a>
     </li>
     <li class="list-inline-item">
-        <a class="btn btn-sm btn-icon btn-soft-secondary" href="#">
+        <a class="btn btn-sm btn-icon btn-soft-secondary" href="https://twitter.com/RAKwireless">
             <span class="fab fa-twitter btn-icon__inner"></span>
         </a>
     </li>
     <li class="list-inline-item">
-        <a class="btn btn-sm btn-icon btn-soft-secondary" href="#">
-            <span class="fab fa-github btn-icon__inner"></span>
+        <a class="btn btn-sm btn-icon btn-soft-secondary" href="https://www.youtube.com/channel/UCy47vnqsUPxd9fn9RP5z54w">
+            <span class="fab fa-youtube btn-icon__inner"></span>
         </a>
     </li>
 </ul>
