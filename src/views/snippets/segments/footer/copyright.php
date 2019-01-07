ae58b5ac2726924baa8c39d24b37fac86620bd8c
Phoenix Eve Aspacio <aspaciop@gmail.com>
2019-01-07 22:13:41 +0800

    copyright

--- src/views/snippets/segments/footer/copyright.php
+++ src/views/snippets/segments/footer/copyright.php
@@ -1,3 +1,4 @@
 <div class="col-6 text-right">
-    <p class="small mb-0">&copy; <?=date('Y');?> Shenzhen Rakwireless Technology Co., Ltd.</p>
+    <p class="small mb-0">&copy; <?=date('Y');?> Shenzhen Rakwireless Technology Co., Ltd.<br>All Rights Reserved. 粤ICP备16112976号</p>
+    <p class="small mb-0">Made with ❤ by Phoenix Eve Aspacio</p>
 </div>
