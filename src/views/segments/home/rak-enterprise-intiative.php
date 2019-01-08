7eff8d2441de1fc1ee9321518b4d9ee130386c9c
Phoenix Eve Aspacio <aspaciop@gmail.com>
2019-01-08 17:42:11 +0800

    space 1

--- src/views/segments/home/rak-enterprise-intiative.php
+++ src/views/segments/home/rak-enterprise-intiative.php
@@ -1,10 +1,9 @@
 <div class="u-devices-v2" style="background-image:url('/assets/img/rakwireless/nasa-53884-unsplash.jpg');background-size:cover;background-attachment:fixed;">
-      <div class="container space-2 space-md-3 position-relative z-index-2">
+      <div class="container space-1 space-md-2 position-relative z-index-2">
         <!-- Title -->
         <div class="w-md-80 w-lg-50 text-center mx-md-auto">
           
-          <h2 class="h1 text-white"><span class="font-weight-semi-bold">We're building up</span><br>
-          an Enterprise Initiative</h2>
+          <h2 class="h1 text-white"><span class="font-weight-semi-bold">We're building up</span> an Enterprise Initiative</h2>
           <p class="lead text-white-70">
           By helping you build your business in scale, customizability through hardware and software integration you're helping us attain our vision.
           </p>
