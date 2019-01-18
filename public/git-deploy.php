5c87b6affec3def32d77d100f7c5705f30256810
Phoenix Eve Aspacio <aspaciop@gmail.com>
2019-01-19 02:11:58 +0800

    deploy

--- public/git-deploy.php
+++ public/git-deploy.php
@@ -2,18 +2,16 @@
 
 error_reporting(0);
 
-if (!isset($_SERVER['HTTP_X_GITHUB_EVENT']) || !isset($_SERVER['HTTP_X_HUB_SIGNATURE'])) {
+$github_event = $_SERVER['HTTP_X_GITHUB_EVENT'];
+$hub_signature = $_SERVER['HTTP_X_HUB_SIGNATURE'];
+$deploy_secret = $_SERVER['DEPLOY_SECRET'];
+$php_input = file_get_contents("php://input");
+$sha1_hmac_hash = hash_hmac('sha1', $php_input, $deploy_secret);
+
+if (!empty($github_event) && !empty($hub_signature) && $hub_signature == 'sha1='.$sha1_hmac_hash && $github_event == 'push') {
+    header('Content-Type: text/plain');
+    echo shell_exec("git pull --recurse-submodules && git submodule init && git submodule update");
+} else {
     http_response_code(400);
     exit;
 }
-
-$deploy_signature = 'sha1='.hash_hmac('sha1', file_get_contents("php://input"), $_SERVER['DEPLOY_SECRET']);
-$deploy_event = 'push';
-
-if ($_SERVER['HTTP_X_HUB_SIGNATURE'] != $deploy_signature || $_SERVER['HTTP_X_GITHUB_EVENT'] != $deploy_event) {
-    http_response_code(400);
-    exit;
-}
-
-header('Content-Type: text/plain');
-echo shell_exec("git pull --recurse-submodules && git submodule init && git submodule update");
