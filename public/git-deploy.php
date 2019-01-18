8aaf9e77c5421345146eb1e896d5daadf750c68f
Phoenix Eve Aspacio <aspaciop@gmail.com>
2019-01-18 14:56:49 +0800

    deploy

--- public/git-deploy.php
+++ public/git-deploy.php
@@ -1,6 +1,18 @@
 <?php
 
+$deploy_signature = 'sha1='.hash_hmac('sha1', file_get_contents("php://input"), $_SERVER['DEPLOY_SECRET']);
+$deploy_event = 'push';
+
+if (!isset($_SERVER['HTTP_X_GITHUB_EVENT']) || !isset($_SERVER['HTTP_X_HUB_SIGNATURE'])) {
+    http_send_status(400);
+    exit;
+}
+
+if ($_SERVER['HTTP_X_HUB_SIGNATURE'] != $deploy_signature) {
+    http_send_status(403);
+    exit;
+}
+
 header('Content-Type: text/plain');
 
-file_put_contents('deploy.txt', print_r($_SERVER));
 echo shell_exec("git pull");
