18f50216abbd9b2407da5ab6e4a335b8ba72c8f5
Phoenix Eve Aspacio <aspaciop@gmail.com>
2019-01-18 15:04:11 +0800

    deploy code fix

--- public/git-deploy.php
+++ public/git-deploy.php
@@ -1,7 +1,7 @@
 <?php
 
 if (!isset($_SERVER['HTTP_X_GITHUB_EVENT']) || !isset($_SERVER['HTTP_X_HUB_SIGNATURE'])) {
-    http_send_status(400);
+    http_response_code(400);
     exit;
 }
 
@@ -9,7 +9,7 @@ $deploy_signature = 'sha1='.hash_hmac('sha1', file_get_contents("php://input"), 
 $deploy_event = 'push';
 
 if ($_SERVER['HTTP_X_HUB_SIGNATURE'] != $deploy_signature) {
-    http_send_status(403);
+    http_response_code(403);
     exit;
 }
 
