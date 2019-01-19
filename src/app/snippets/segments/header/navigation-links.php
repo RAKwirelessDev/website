b6402fff9044eb974231e13cdd9dffd9279bb8e8
Phoenix Eve Aspacio <aspaciop@gmail.com>
2019-01-19 13:18:48 +0800

    nav structure

--- src/app/snippets/segments/header/navigation-links.php
+++ src/app/snippets/segments/header/navigation-links.php
@@ -4,21 +4,21 @@ $data->navigation_links = [
     'Home' => '/',
     'About' => '/about/',
     'Blog' => 'https://en.blog.rakwireless.com/',
-    'Products' => [
-        'Video Module' => '/projects/video-module/',
-        'Video Projects' => '/projects/video-projects/',
-        'IoT Services' => '/projects/iot-services/',
-        'WisLink Cellular' => '/projects/wislink-cellular/',
+    'Solutions' => [
+        'Video Module' => '/solutions/video-module/',
+        'Video Projects' => '/solutions/video-projects/',
+        'IoT Services' => '/solutions/iot-services/',
+        'WisLink Cellular' => '/solutions/wislink-cellular/',
     ],
     'Integrations' => [
         'Apple HomeKit' => '/integrations/apple-homekit/',
         'Amazon Web Services' => '/integrations/amazon-web-services/',
         'Amazon Alexa' => '/integrations/amazon-alexa/'
     ],
-    'Community' => [
-        'Support Center' => 'https://forum.rakwireless.com/',
-        'Download Center' => '/community/download-center/',
-        'Documentation' => '/community/documentation/',
+    'Resources' => [
+        'Documentation' => '/resources/documentation/',
+        'Forum' => 'https://forum.rakwireless.com/',
+        'Downloads' => '/resources/download-center/',
         'GitHub' => 'https://www.github.com/RAKWireless/'
     ]
 ];
