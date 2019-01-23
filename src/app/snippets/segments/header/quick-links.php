cc4783c410b36639913f3da50daaeb011ec9e8ea
Phoenix Eve Aspacio <aspaciop@gmail.com>
2019-01-23 12:19:49 +0800

    redundant

--- src/app/snippets/segments/header/quick-links.php
+++ src/app/snippets/segments/header/quick-links.php
@@ -1,7 +1,6 @@
 <?php
 
 $data->quick_links = [
-    'FAQ' => '/faq/',
     'GitHub' => 'https://www.github.com/RAKWireless/',
     'Contact us' => '/contact-us/',
     'Store' => 'https://store.rakwireless.com/'
