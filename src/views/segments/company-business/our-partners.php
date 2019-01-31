d3cf77c2deb6a6555727572130bb8a83ddc74311
Phoenix Eve Aspacio <aspaciop@gmail.com>
2019-01-31 15:18:56 +0800

    partners link

--- src/views/segments/company-business/our-partners.php
+++ src/views/segments/company-business/our-partners.php
@@ -4,35 +4,34 @@
         <!-- Clients Section -->
         <div class="row justify-content-center text-center">
             <div class="col-4 col-md-4 mb-2">
-                <img class="u-partners" src="/assets/img/rakwireless/ImageGen.png" alt="Image Description">
+                <a href="http://www.nabto.com/"><img class="u-partners" src="/assets/img/rakwireless/ImageGen.png" alt="Nabto"></a>
             </div>
             <div class="col-4 col-md-4 mb-2">
-                <img class="u-partners" src="/assets/img/rakwireless/584828d8cef1014c0b5e49ee.png" alt="Image Description">
+                <a href="http://z-wavealliance.org/"><img class="u-partners" src="/assets/img/rakwireless/584828d8cef1014c0b5e49ee.png" alt="Z-Wave"></a>
             </div>
             <div class="col-4 col-md-4 mb-2">
-                <img class="u-partners" src="/assets/img/rakwireless/amazon-alexa-logo-vector-png-a-year-ago-we-launched-the-alexa-skills-kit-to-allow-developers-to-build-new-voice-2825.png"
-                    alt="Image Description">
-            </div>
-
-            <div class="w-100"></div>
-
-            <div class="col-4 col-md-4 mb-2">
-                <img class="u-partners" src="/assets/img/rakwireless/homekit_icon_1x.png" alt="Image Description">
-            </div>
-            <div class="col-4 col-md-4 mb-2">
-                <img class="u-partners" src="/assets/img/rakwireless/amazon-web-services.svg" alt="Image Description">
+                <a href="http://www.rakwireless.com/cn/eco_chain/alexa"><img class="u-partners" src="/assets/img/rakwireless/amazon-alexa-logo-vector-png-a-year-ago-we-launched-the-alexa-skills-kit-to-allow-developers-to-build-new-voice-2825.png" alt="Amazon Alexa"></a>
             </div>
 
             <div class="w-100"></div>
 
             <div class="col-4 col-md-4 mb-2">
-                <img class="u-partners" src="/assets/img/rakwireless/actility-logo.png" alt="Image Description">
+                <a href="http://www.rakwireless.com/cn/eco_chain/homekit"><img class="u-partners" src="/assets/img/rakwireless/homekit_icon_1x.png" alt="Apple HomeKit"></a>
             </div>
             <div class="col-4 col-md-4 mb-2">
-                <img class="u-partners" src="/assets/img/rakwireless/partner-logo-mydevices.webp" alt="Image Description">
+                <a href="http://www.rakwireless.com/cn/eco_chain/aws"><img class="u-partners" src="/assets/img/rakwireless/amazon-web-services.svg" alt="Amazon Web Services"></a>
+            </div>
+
+            <div class="w-100"></div>
+
+            <div class="col-4 col-md-4 mb-2">
+                <a href="https://www.actility.com/"><img class="u-partners" src="/assets/img/rakwireless/actility-logo.png" alt="Actility"></a>
             </div>
             <div class="col-4 col-md-4 mb-2">
-                <img class="u-partners" src="/assets/img/rakwireless/Thread-wordmark-wht.svg" alt="Image Description">
+                <a href="https://mydevices.com/"><img class="u-partners" src="/assets/img/rakwireless/partner-logo-mydevices.webp" alt="My Devices"></a>
+            </div>
+            <div class="col-4 col-md-4 mb-2">
+                <a href="http://threadgroup.org/"><img class="u-partners" src="/assets/img/rakwireless/Thread-wordmark-wht.svg" alt="Thread"></a>
             </div>
         </div>
     </div>
