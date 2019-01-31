74adbfc45b84c9bd14dc1b180377ca219aca7553
Phoenix Eve Aspacio <aspaciop@gmail.com>
2019-01-31 12:40:25 +0800

    update

--- src/app/segments/home/images-carousel.php
+++ src/app/segments/home/images-carousel.php
@@ -6,8 +6,8 @@ $data->carousel = [
         'description' => '- Raspberry Pi 3B+<br>- LoRaWAN 1.0.2<br>- Android & iOS app<br>- Connect to IoT in a Box™<br>- Alert, user management, maps and GPS<br>- Custom integration services, etc.',
         'images' => [
             '/assets/img/rakwireless/pilot_gw.png',
-            '/assets/img/rakwireless/02.png',
-            '/assets/img/rakwireless/03.png'
+            //'/assets/img/rakwireless/02.png',
+            //'/assets/img/rakwireless/03.png'
         ],
         'store_link' => 'https://www.aliexpress.com/store/product/Pilot-Gate-way-connect-to-the-TTN-Network-Server-and-IoT-in-a-Box-application-cloud/2805180_32951841630.html?spm=a2g1y.12024536.productList_11474674.pic_5',
         'slide_class' => 'gradient-half-semi-violet'
@@ -17,8 +17,8 @@ $data->carousel = [
         'description' => '- Based on Semtech\'s SX1301<br>- Frequency: 443MHz/868MHz/915MHz<br>- 8 uplink channels for Makers<br>- 1 downlink channel for Makers',
         'images' => [
             '/assets/img/rakwireless/01_small.png',
-            '/assets/img/rakwireless/04.png',
-            '/assets/img/rakwireless/02 (1).png'
+            //'/assets/img/rakwireless/04.png',
+            //'/assets/img/rakwireless/02 (1).png'
         ],
         'store_link' => 'https://www.aliexpress.com/store/product/RAK831-LoRa-LoRaWAN-Gateway-Module-base-on-SX1301-433-868-915MHz-range-of-up-to-49200ft/2805180_32821411294.html?spm=2114.12010608.0.0.zaMjhY%20target=',
         'slide_class' => 'gradient-half-warning-v3'
@@ -29,7 +29,7 @@ $data->carousel = [
         'images' => [
             '/assets/img/rakwireless/RAK5205-1_1217 (1).png'
         ],
-        'slide_class' => 'gradient-half-warning-v3'
+        'slide_class' => 'gradient-half-semi-violet'
     ],
     'WisLink Cellular' => [
         'tag_line' => 'BG96 Arduino Shield',
