5696e8c49c710fc1c7dac9378e234ed9f19ab50b
Phoenix Eve Aspacio <aspaciop@gmail.com>
2019-01-31 12:13:14 +0800

    daily checkout

--- src/views/segments/company-business/locations.php
+++ src/views/segments/company-business/locations.php
@@ -2,24 +2,65 @@
 
     <h2 class="text-primary mx-auto mw-mc mb-6">Office <strong class="font-weight-semi-bold">Locations</strong></h2>
     <div class="row justify-content-lg-between">
-        <!-- Left Content -->
-        <div class="col-lg-6 mb-5 mb-md-0">
-            <h2 class="h5 mx-auto mw-mc"><strong class="font-weight-semi-bold">Shenzhen HQ</strong></h2>
-            <span>Room 506, Bldg B, New Compark, Pingshan First Road, Taoyuan Street, XiLi town Nanshan District,
-                Shenzhen, Chispana</span>
-            <br><span>Email: ken.yu@rakwireless.com</span>
-            <br><span>Contact number: 0755-86108311, 0755-26506594</span>
-            <br><span>Fax: 0755-86152201</span>
-            <img class="img-fluid" src="/assets/img/rakwireless/company_address_1.jpg" />
+
+        <div class="col-sm-6 col-md-5 col-lg-4">
+            <h4 class="h6">Shenzhen Headquarters</h4>
+
+            <!-- Address -->
+            <address
+                class="list-group list-group-transparent list-group-flush list-group-borderless mb-0">
+                <span class="list-group-item">
+                    Room 506, Bldg B, New Compark, Pingshan First Road, Taoyuan Street, XiLi Town Nanshan District,
+                    Shenzhen,
+                    China (518000)
+                </span>
+                <a class="list-group-item"
+                    href="mailto:shenzhen@rakwireless.com">shenzhen@rakwireless.com</a>
+                <span class="list-group-item">
+                    TEL: 0755-86108311, 0755-26506594<br>
+                    Fax: 0755-86152201
+                </span>
+            </address>
+            <!-- End Address -->
         </div>
 
-        <!-- Right Content -->
-        <div class="col-lg-6 mb-5 mb-md-0">
-            <h2 class="h5 mx-auto mw-mc"><strong class="font-weight-semi-bold">Beijing Office</strong></h2>
-            <span>Room 306, Building No. 3, MeiTang Commercial Building, Kexing west Road, Changping District, Beijing,
-                P.R. Chispana</span>
-            <br><span>Email: gang.chen@rakwireless.com</span>
-            <img class="img-fluid" src="/assets/img/rakwireless/company_address_3.png" />
+        <div class="col-sm-6 col-md-5 col-lg-4">
+            <h4 class="h6">Shanghai R&D Center</h4>
+
+            <!-- Address -->
+            <address
+                class="list-group list-group-transparent list-group-flush list-group-borderless mb-0">
+                <span class="list-group-item">
+                    East room 12304, Greenland Territorial Sea Building, Jinye Road, High-Tech Zone, Xi’an, Shangxi
+                    Province,
+                    China (710000)
+                </span>
+                <a class="list-group-item"
+                    href="mailto:shanghai@rakwireless.com">shanghai@rakwireless.com</a>
+                <span class="list-group-item">
+                    TEL: 021-61553990<br>
+                    Fax: 021-54721038
+                </span>
+            </address>
+            <!-- End Address -->
         </div>
+
+        <div class="col-sm-6 col-md-5 col-lg-4">
+            <h4 class="h6">Beijing Office</h4>
+
+            <!-- Address -->
+            <address
+                class="list-group list-group-transparent list-group-flush list-group-borderless mb-0">
+                <span class="list-group-item">
+                    Room 306, Building No. 3, MeiTang Commercial Building, Kexing west Road, Changping District,
+                    Beijing, China
+                    (100085)
+                </span>
+                <a class="list-group-item"
+                    href="mailto:beijing@rakwireless.com">beijing@rakwireless.com</a>
+            </address>
+            <!-- End Address -->
+        </div>
+
     </div>
 </div>
--- src/views/segments/company-business/our-partners.php
+++ src/views/segments/company-business/our-partners.php
@@ -3,35 +3,35 @@
         <h2 class="text-primary mx-auto mw-mc mb-6">Our <strong class="font-weight-semi-bold">Partners</strong></h2>
         <!-- Clients Section -->
         <div class="row justify-content-center text-center">
-            <div class="col-4 col-md-3 mb-2">
+            <div class="col-4 col-md-4 mb-2">
                 <img class="u-partners" src="/assets/img/rakwireless/ImageGen.png" alt="Image Description">
             </div>
-            <div class="col-4 col-md-3 mb-2">
+            <div class="col-4 col-md-4 mb-2">
                 <img class="u-partners" src="/assets/img/rakwireless/584828d8cef1014c0b5e49ee.png" alt="Image Description">
             </div>
-            <div class="col-4 col-md-3 mb-2">
+            <div class="col-4 col-md-4 mb-2">
                 <img class="u-partners" src="/assets/img/rakwireless/amazon-alexa-logo-vector-png-a-year-ago-we-launched-the-alexa-skills-kit-to-allow-developers-to-build-new-voice-2825.png"
                     alt="Image Description">
             </div>
 
             <div class="w-100"></div>
 
-            <div class="col-4 col-md-3 mb-2">
+            <div class="col-4 col-md-4 mb-2">
                 <img class="u-partners" src="/assets/img/rakwireless/homekit_icon_1x.png" alt="Image Description">
             </div>
-            <div class="col-4 col-md-3 mb-2">
+            <div class="col-4 col-md-4 mb-2">
                 <img class="u-partners" src="/assets/img/rakwireless/amazon-web-services.svg" alt="Image Description">
             </div>
 
             <div class="w-100"></div>
 
-            <div class="col-4 col-md-3 mb-2">
+            <div class="col-4 col-md-4 mb-2">
                 <img class="u-partners" src="/assets/img/rakwireless/actility-logo.png" alt="Image Description">
             </div>
-            <div class="col-4 col-md-3 mb-2">
+            <div class="col-4 col-md-4 mb-2">
                 <img class="u-partners" src="/assets/img/rakwireless/partner-logo-mydevices.webp" alt="Image Description">
             </div>
-            <div class="col-4 col-md-3 mb-2">
+            <div class="col-4 col-md-4 mb-2">
                 <img class="u-partners" src="/assets/img/rakwireless/Thread-wordmark-wht.svg" alt="Image Description">
             </div>
         </div>
