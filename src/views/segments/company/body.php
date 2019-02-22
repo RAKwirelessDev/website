7052096d2d243166852fdc80b3ef496fd0c8a799
RAKwebsite <46487341+RAKwebsite@users.noreply.github.com>
2019-02-22 11:37:30 +0800

    Update body.php

--- src/views/segments/company/body.php
+++ src/views/segments/company/body.php
@@ -7,11 +7,12 @@
               <div class="col-lg-6 dp-ig">
                 <a href="/company/business/">Company Background &raquo;</a>
                 <a href="/company/history/">Company History &raquo;</a>
-                <a href="#">Timeline Innovations &raquo;</a>
+                <a href="#">Timeline of Innovations &raquo;</a>
                 <a href="#">Core Engineering Team &raquo;</a>
               </div>
               <div class="col-lg-6 dp-ig">
-                <a href="#">Partners and Distributors &raquo;</a>
+                <a href="#">Business Partners &raquo;</a>
+                <a href="#">Value Added Resellers &raquo;</a>
                 <a href="#">Headquarters and Offices &raquo;</a>
                 <a href="#">News &raquo;</a>
               </div>
@@ -24,11 +25,10 @@
               <div class="col-lg-6 dp-ig">
                 <a href="#">Approach to Quality &raquo;</a>
                 <a href="#">Hardware Design Capability &raquo;</a>
-                <a href="#">R&D Quality Control System &raquo;</a>
-              </div>
-              <div class="col-lg-6 dp-ig">
                 <a href="#">RAK Blocks Portfolio &raquo;</a>
                 <a href="#">RUI - IoT Lego Club &raquo;</a>
+              </div>
+              <div class="col-lg-6 dp-ig">
                 <a href="#">Projects and Case Studies &raquo;</a>
               </div>
             </div>
